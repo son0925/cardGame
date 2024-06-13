44bf69e3e89a3dff27d57bf998a9c361a3495a82
son0925 <shs00925@naver.com>
2024-06-13 13:45:57 +0900

    약간의 수정 및 업데이트

--- src/server.js
+++ src/server.js
@@ -39,6 +39,13 @@ app.use(cookieSession({
 app.use(express.static(path.join(__dirname, '../public')));
 
 
+// socket 미들웨어를 만들어서 유저 이름 받아오기
+// socket에 상대방 유저 접속시 이벤트 만들기
+// 이벤트
+// 1. 상대 이름, 돈 정보 보내기
+// 2. 게임 시작 버튼 활성, 비활성
+// 3. 게임 알고리즘 구현하기
+// 4. 나갔을 때 이벤트 만들기
 io.on('connection', (socket) => {
   console.log('유저 접속');
 
